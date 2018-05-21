7b336609663297554d05f689abf3416657a08df0
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-21 20:56:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const listItems = document.querySelectorAll('.ranked-list li');
+  
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].innerHTML = parseInt(listItems[i].innerHTML) + n;
+  }
+  
+  return listItems;
+}
+
+function deepestChild() {
+  let deepestChild;
+  const divs = document.querySelector('#grand-node').querySelectorAll('div');
+
+  for (let i = 0; i < divs.length; i++) {
+    if (divs[i].childElementCount === 0) {
+    	deepestChild = divs[i];
+    }
+  }
+  
+  return deepestChild;
+}
