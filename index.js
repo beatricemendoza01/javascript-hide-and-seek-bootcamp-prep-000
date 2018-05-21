e776083e091cfe5ccf5578946029dfb590bae913
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-21 20:37:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const listItems = document.querySelectorAll('.ranked-list li');
+  
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].innerHTML = parseInt(listItems[i].innerHTML) + n;
+  }
+  
+  return listItems;
+}
+
+// function deepestChild() {
+  
+// }
